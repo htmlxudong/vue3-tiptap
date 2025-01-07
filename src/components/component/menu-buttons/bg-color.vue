0d412cf60d8fa47ac590150ea7ed5d72cbe140f8
xudong <2394397242@qq.com>
2025-01-07 11:01:21 +0800

    12

--- src/components/component/menu-buttons/bg-color.vue
+++ src/components/component/menu-buttons/bg-color.vue
@@ -16,9 +16,25 @@
 					<div class="color-box" :style="{ background: color }"></div>
 				</li>
 			</ul>
+
+			<a-popover placement="right">
+				<template #content>
+					<ColorPicker @updateColor="onUpdateColor" />
+				</template>
+				<div class="color-more">
+					<div class="color-circle">
+						<Icon name="color-picker" />
+						更多颜色
+					</div>
+					<div>
+						<Icon name="caret-right" />
+					</div>
+				</div>
+			</a-popover>
+
 		</template>
 		<a-tooltip placement="top">
-			<template #title> <span>文字颜色</span> </template>
+			<template #title> <span>背景颜色</span> </template>
 			<div
 				:class="[
 					'tools__button',
@@ -39,15 +55,28 @@
 import { getTextColor } from "./cells";
 import { ref, reactive } from "vue";
 import { BgColorsOutlined, CaretDownOutlined } from "@ant-design/icons-vue";
+import ColorPicker from "../../component/color-picker/color-picker.vue";
+import Icon from "../../component/Icon/src/Icon.vue";
+
 const props = defineProps(["editor"]);
 const colors = reactive(getTextColor());
-
+const currentColor = ref("#ffffff");
 const currentIndex = ref(9);
 
 const toggleColor = (color, index) => {
 	currentIndex.value = index;
 	props.editor.chain().focus().setHighlight({ color: color }).run();
 };
+
+const onUpdateColor = color => {
+	currentColor.value = `#${color}`;
+	props.editor
+		.chain()
+		.focus()
+		.setHighlight({ color: `#${color}` })
+		.run();
+};
+
 </script>
 
 <style lang="scss" scoped>
@@ -55,6 +84,7 @@ const toggleColor = (color, index) => {
 	border: 1px solid rgba(0, 0, 0, 0.6) !important;
 }
 .colors-palette {
+	list-style: none;
 	width: 230px;
 	display: flex;
 	flex-wrap: wrap;
@@ -74,6 +104,24 @@ const toggleColor = (color, index) => {
 			width: 17px;
 			height: 17px;
 		}
+	}
+}
+.color-more {
+	cursor: pointer;
+	width: 100%;
+	padding: 5px 0;
+	border-top: 1px solid #eee;
+	display: flex;
+	justify-content: space-between;
+	background: transparent;
+	transition: background 0.3s;
+	&:hover {
+		background: #e8e8e8;
+	}
+	.color-circle {
+		display: flex;
+		column-gap: 5px;
+		align-items: center;
 	}
 }
 </style>
