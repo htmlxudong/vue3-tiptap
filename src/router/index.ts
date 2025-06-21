ce3d452ce4d0946f767d3e5915a738b74177b897
xudong <2394397242@qq.com>
2025-06-21 21:37:24 +0800

    fix:修复了一些bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,8 @@ import HomeView from '@/views/home/home.vue'
 import VModelTest from '@/views/v-model-test.vue'
 
 const routes = [
-  { path: '/', component: DemoVuew },
-  { path: '/home', component: HomeView },
+  { path: '/', component: HomeView },
+  { path: '/home', component: DemoVuew },
   { path: '/v-model-test', component: VModelTest },
 ]
 
